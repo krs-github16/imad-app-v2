41c890f7f2295513b8468ff49179e728e0bbf246
krs-github16 <ikrs.me.dp@gmail.com>
2017-02-19 08:30:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/krs-one', function (req, res) {
+  res.send('article one is requested and wil be served shortly');
+});
+
+app.get('/krs-two', function (req, res) {
+  res.send('article two is requested and wil be served shortly');
+});
+
+app.get('/krs-three', function (req, res) {
+  res.send('article three is requested and wil be served shortly');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
